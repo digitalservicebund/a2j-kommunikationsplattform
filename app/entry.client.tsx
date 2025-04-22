7c37d7e18185bab5269828208c9adf85731bb831
Pierre Marais <pierre.marais@digitalservice.bund.de>
2025-04-22 09:16:17 +0200

    Ignore health check in sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -18,6 +18,7 @@ if (SENTRY_DSN !== undefined) {
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
     tracePropagationTargets: [
+      /^(?!\/\.well-known\/health$).*$/, // Exclude /.well-known/health
       /^\//, //  This enables trace propagation for all relative paths on the same domain.
       /^https:\/\/kompla\.sinc\.de\//,
     ],
--- app/instrument.server.mjs
+++ app/instrument.server.mjs
@@ -3,4 +3,9 @@ import * as Sentry from "@sentry/react-router";
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0, // Capture 100% of the transactions
+  tracePropagationTargets: [
+    /^(?!\/\.well-known\/health$).*$/, // Exclude /.well-known/health
+    /^\//, // This enables trace propagation for all relative paths on the same domain.
+    /^https:\/\/kompla\.sinc\.de\//,
+  ],
 });
