9bf3ce90ce471c7e7b95c996a1e79ad4b95e6bfd
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-07-07 18:20:24 +0200

    Refactor example a11y test
    Part of https://digitalservicebund.atlassian.net/browse/KOMPLA-486

--- tests/a11y/expectPageToBeAccessible.spec.ts
+++ tests/a11y/expectPageToBeAccessible.spec.ts
@@ -1,8 +1,8 @@
 import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
-test.describe("index", () => {
-  test("should not have any automatically detectable accessibility issues", async ({
+test.describe("a11y", () => {
+  test("homepage should not have any automatically detectable accessibility issues", async ({
     page,
   }) => {
     await page.goto("/");
