161f95a40a910484ef4c4596f5ef1d544bc26ee3
chris <earthaccount+gh@mailbox.org>
2025-07-15 17:54:57 +0200

    Reduce Sentry sampling
    Lets reduce the sampling rate in an effort to not run out of quota

--- app/instrument.server.mjs
+++ app/instrument.server.mjs
@@ -2,5 +2,5 @@ import * as Sentry from "@sentry/react-router";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  tracesSampleRate: 0.1,
 });
