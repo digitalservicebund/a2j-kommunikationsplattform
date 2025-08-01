f30ea8e44ea2f718f75038ef7b5e4455ae2eb46c
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2025-08-01 16:47:20 +0200

    Fix missing title at login page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,6 +7,7 @@ import {
 } from "@kern-ux-annex/kern-react-kit";
 import { Link, useLoaderData } from "react-router";
 import { NarrowPageLayout } from "~/components/NarrowPageLayout";
+import { PageMetadata } from "~/components/PageMetadata";
 import { config } from "~/config/config";
 
 export async function loader() {
@@ -18,56 +19,59 @@ export default function IndexPage() {
   const { environment } = useLoaderData<typeof loader>();
 
   return (
-    <NarrowPageLayout>
-      <div className="login">
-        <div className="login__container">
-          <div className="logo">
-            <KernIcon icon="network_node" size="large" />
-            <KernHeading size="large" level={1}>
-              Kommunikationsplattform
-            </KernHeading>
-          </div>
+    <>
+      <PageMetadata />
+      <NarrowPageLayout>
+        <div className="login">
+          <div className="login__container">
+            <div className="logo">
+              <KernIcon icon="network_node" size="large" />
+              <KernHeading size="large" level={1}>
+                Kommunikationsplattform
+              </KernHeading>
+            </div>
 
-          <KernDivider />
+            <KernDivider />
 
-          <KernText type="subline">
-            Willkommen auf der Pilotplattform für den digitalen Austausch
-            zwischen Gerichten und Verfahrensbeteiligten.
-          </KernText>
+            <KernText type="subline">
+              Willkommen auf der Pilotplattform für den digitalen Austausch
+              zwischen Gerichten und Verfahrensbeteiligten.
+            </KernText>
 
-          <div className="login__buttons">
-            <Link
-              to="/login"
-              className="kern-btn kern-btn--block kern-btn--primary"
-            >
-              <span className="kern-label">Anmeldung Anwaltschaft</span>
-            </Link>
-
-            <KernButton
-              text="Anmeldung Gerichte"
-              variant="primary"
-              disabled
-              className="kern-btn--block"
-            />
-
-            {/* only render "Testzugang" demo link for non production environments */}
-            {environment !== "production" && (
+            <div className="login__buttons">
               <Link
-                to="/prototype/verfahren"
-                className="kern-btn kern-btn--block kern-btn--secondary"
-                onClick={() => {
-                  document.cookie = "demoMode=true; path=/; max-age=3600"; // 1 hour
-                }}
-                data-testid="demoBtn"
+                to="/login"
+                className="kern-btn kern-btn--block kern-btn--primary"
               >
-                <span className="kern-label">Testzugang</span>
+                <span className="kern-label">Anmeldung Anwaltschaft</span>
               </Link>
-            )}
-          </div>
 
-          <KernDivider />
+              <KernButton
+                text="Anmeldung Gerichte"
+                variant="primary"
+                disabled
+                className="kern-btn--block"
+              />
+
+              {/* only render "Testzugang" demo link for non production environments */}
+              {environment !== "production" && (
+                <Link
+                  to="/prototype/verfahren"
+                  className="kern-btn kern-btn--block kern-btn--secondary"
+                  onClick={() => {
+                    document.cookie = "demoMode=true; path=/; max-age=3600"; // 1 hour
+                  }}
+                  data-testid="demoBtn"
+                >
+                  <span className="kern-label">Testzugang</span>
+                </Link>
+              )}
+            </div>
+
+            <KernDivider />
+          </div>
         </div>
-      </div>
-    </NarrowPageLayout>
+      </NarrowPageLayout>
+    </>
   );
 }
