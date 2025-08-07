195ae467dcb277ca5c30bc91b71ef1d122ed6e8e
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-08-07 11:36:25 +0200

    Add KernSpace comp arount KernAlert

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,7 @@ import {
   KernDivider,
   KernHeading,
   KernIcon,
+  KernSpace,
   KernText,
 } from "@kern-ux-annex/kern-react-kit";
 import { Link, useLoaderData, useSearchParams } from "react-router";
@@ -31,12 +32,16 @@ export default function IndexPage() {
 
       <NarrowPageLayout>
         {alertStatus === LogoutStatus.Auto && (
-          // @TODO: needs to get correct spacings @ top/bottom
-          <KernAlert
-            body="Aus Sicherheitsgründen wurden Sie nach 60 Minuten Inaktivität automatisch abgemeldet. Bitte melden Sie sich erneut an."
-            text="Automatisch abgemeldet"
-            variant="warning"
-          />
+          // @TODO: needs to get correct spacings @ top/bottom, bug @kern-ux-annex/kern-react-kit v2.16.0
+          <>
+            <KernSpace size="xl" />
+            <KernAlert
+              body="Aus Sicherheitsgründen wurden Sie nach 60 Minuten Inaktivität automatisch abgemeldet. Bitte melden Sie sich erneut an."
+              text="Automatisch abgemeldet"
+              variant="warning"
+            />
+            <KernSpace size="m" />
+          </>
         )}
 
         <div className="login">
