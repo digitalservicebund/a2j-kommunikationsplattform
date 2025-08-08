7eefacde48086fad6f44675aa8cc253167323967
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-08-08 17:17:44 +0200

    Add logout by user functionality
    - Renamed LogoutStatus to LogoutType
    - Updated AlertState with LogoutType.Automatic and LogoutType.ByUser handling
    - Added logoutType within /action.logout-user route to distinguish between the logout options

--- app/routes/action.logout-user.tsx
+++ app/routes/action.logout-user.tsx
@@ -4,14 +4,23 @@ import {
   getSession,
 } from "~/services/prototype.session.server";
 
-export enum LogoutStatus {
-  Auto = "logged-out",
+export enum LogoutType {
+  Automatic = "auto-logged-out",
+  ByUser = "logged-out",
 }
 
+/**
+ * /action/logout-user
+ *
+ * Redirects to the login page with a automatic or
+ */
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const logoutType = formData.get("logoutType") as LogoutType;
+
   const session = await getSession(request.headers.get("Cookie"));
 
-  return redirect(`/?status=${LogoutStatus.Auto}`, {
+  return redirect(`/?status=${logoutType}`, {
     headers: {
       "Set-Cookie": await destroySession(session),
     },
