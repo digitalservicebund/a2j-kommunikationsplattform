0811c0512cd57a43ffa323d3eec814f0b173fce5
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-08-18 18:46:32 +0200

    Add unit tests for layout components

--- app/layouts/NarrowLayout.tsx
+++ app/layouts/NarrowLayout.tsx
@@ -5,7 +5,10 @@ export default function NarrowLayout() {
   return (
     <div className="kern-container">
       <div className="kern-row kern-justify-content-center">
-        <div className="kern-col-sm-10 kern-col-md-8 kern-col-lg-6 kern-col-xxl-4">
+        <div
+          className="kern-col-sm-10 kern-col-md-8 kern-col-lg-6 kern-col-xxl-4"
+          data-testid="narrow-layout"
+        >
           <main>
             <Outlet />
           </main>
