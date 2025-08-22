0f97796845e35444bf5908ad5798f14d12761035
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-08-22 13:45:16 +0200

    Remove redundant beforeEach in Kopfzeile tests

--- tests/e2e/kopfzeile.spec.ts
+++ tests/e2e/kopfzeile.spec.ts
@@ -1,10 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Kopfzeile (rendered for all pages)", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/");
-  });
-
   test("should render Kopfzeile on every page", async ({ page }) => {
     await page.goto("/");
     await expect(page.locator(".kern-kopfzeile__flagge")).toBeVisible();
