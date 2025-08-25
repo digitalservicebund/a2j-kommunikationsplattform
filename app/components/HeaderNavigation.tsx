65d4467c58d9c60f25e93c03c5e8b8dbed37f1bc
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-08-25 17:31:11 +0200

    Refactor HeaderNavigation DRY

--- app/components/HeaderNavigation.tsx
+++ app/components/HeaderNavigation.tsx
@@ -1,59 +1,67 @@
 import { Form } from "react-router";
 import { LogoutType } from "~/routes/action.logout-user";
 
+export function LogoutButton() {
+  return (
+    <Form method="post" action="/action/logout-user">
+      <input type="hidden" name="logoutType" value={LogoutType.ByUser} />
+      <button type="submit" className="kern-link logout-button">
+        <span
+          className="kern-icon kern-icon--close kern-icon--default"
+          aria-hidden="true"
+        ></span>
+        <span aria-hidden="true">Abmelden</span>
+      </button>
+    </Form>
+  );
+}
+
+const navigationLinksList = [
+  {
+    name: "Verfahren",
+    iconName: "kern-icon--home",
+    label: "Verfahren",
+    url: "#",
+  },
+  {
+    name: "Mitteilungen",
+    iconName: "kern-icon--mail",
+    label: "Mitteilungen",
+    url: "#",
+  },
+  {
+    name: "Kalender",
+    iconName: "kern-icon--calendar-today",
+    label: "Kalender",
+    url: "#",
+  },
+  {
+    name: "Profil",
+    iconName: "kern-icon--easy-language",
+    label: "Profil",
+    url: "#",
+  },
+];
+
 export default function HeaderNavigation() {
   return (
     <nav>
       <ul className="kern-task-list__list navigation-list">
-        <li>
-          <a href="#" className="kern-link">
-            <span
-              className="kern-icon kern-icon--home kern-icon--default"
-              aria-hidden="true"
-            ></span>
-            Verfahren
-          </a>
-        </li>
-        <li>
-          <a href="#" className="kern-link">
-            <span
-              className="kern-icon kern-icon--mail kern-icon--default"
-              aria-hidden="true"
-            ></span>
-            Mitteilungen
-          </a>
-        </li>
-        <li>
-          <a href="#" className="kern-link">
-            <span
-              className="kern-icon kern-icon--calendar-today kern-icon--default"
-              aria-hidden="true"
-            ></span>
-            Kalender
-          </a>
-        </li>
-        <li>
-          <a href="#" className="kern-link">
-            <span
-              className="kern-icon kern-icon--easy-language kern-icon--default"
-              aria-hidden="true"
-            ></span>
-            Profil
-          </a>
-        </li>
-        <li>
-          <Form method="post" action="/action/logout-user">
-            <input type="hidden" name="logoutType" value={LogoutType.ByUser} />
-            <button type="submit" className="kern-link logout-button">
+        {navigationLinksList.map((link) => (
+          <li key={link.name}>
+            <a href={link.url} className="kern-link">
               <span
-                className="kern-icon kern-icon--close kern-icon--default"
+                className={`kern-icon ${link.iconName} kern-icon--default`}
                 aria-hidden="true"
               ></span>
-              <span className="" aria-hidden="true">
-                Abmelden
-              </span>
-            </button>
-          </Form>
+              {link.label}
+            </a>
+          </li>
+        ))}
+        <li>
+          <li>
+            <LogoutButton />
+          </li>
         </li>
       </ul>
     </nav>
