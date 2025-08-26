917a4a75384ad7db67e2d0c4472638f500b65969
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-08-26 14:17:37 +0200

    Simplify DefaultLayout component

--- app/layouts/default-layout.tsx
+++ app/layouts/default-layout.tsx
@@ -3,10 +3,7 @@ import PageFooter from "~/components/PageFooter";
 
 export default function DefaultLayout() {
   return (
-    <div
-      className="kern-container vertical-padding-x-large"
-      data-testid="default-layout"
-    >
+    <div className="kern-container" data-testid="default-layout">
       <main>
         <Outlet />
       </main>
