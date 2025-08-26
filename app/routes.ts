f0668e7fc3ddd6c91296a168280f993f8497c025
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-08-26 15:54:56 +0200

    Refactor e2e tests

--- app/routes.ts
+++ app/routes.ts
@@ -11,7 +11,7 @@ export default [
 
   layout("./layouts/default-layout.tsx", [
     // verfahren
-    route("verfahrensUebersicht", "./routes/verfahrensUebersicht.tsx"),
+    route("verfahrens-uebersicht", "./routes/verfahrensUebersicht.tsx"),
     ...prefix("prototype/verfahren", [
       index("./routes/prototype.verfahren.tsx"),
       route(":id", "./routes/prototype.verfahren.$id.tsx", {}),
--- tests/e2e/verfahrensUebersicht.spec.ts
+++ tests/e2e/verfahrensUebersicht.spec.ts
@@ -7,20 +7,20 @@ const navigationLinksList = [
   { label: "Profil" },
 ];
 
-test.describe("Verfahrensübersicht", () => {
+test.describe("verfahrens-uebersicht", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/verfahrens-uebersicht");
+  });
   test("shows logo", async ({ page }) => {
-    await page.goto("/verfahrensuebersicht");
     await expect(page.locator("text=Kommunikationsplattform")).toBeVisible();
   });
   test("shows all navigation links", async ({ page }) => {
-    await page.goto("/verfahrensuebersicht");
     for (const link of navigationLinksList) {
       await expect(page.getByRole("link", { name: link.label })).toBeVisible();
     }
   });
 
   test("shows user profile cell", async ({ page }) => {
-    await page.goto("/verfahrensuebersicht");
     await expect(page.locator("text=Kim Neumann")).toBeVisible();
   });
 });
