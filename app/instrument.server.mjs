89d339326cd6c7f4fe27bdd11db7f4ac38cf5f6a
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-08-29 16:37:28 +0200

    Remove kube-probe UA requests from Sentry tracing
    Docs: https://docs.sentry.io/platforms/javascript/tracing/configure-sampling/#sampling-function-tracessampler
    Ticket: https://digitalservicebund.atlassian.net/browse/KOMMPLA-112

--- app/instrument.server.mjs
+++ app/instrument.server.mjs
@@ -2,5 +2,16 @@ import * as Sentry from "@sentry/react-router";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 0.1, //  Capture 10% of the transactions
+  // Capture 0 percent of transactions originating from kube-probe, e.g. /readyz calls by K8s
+  tracesSampler: (samplingContext) => {
+    const userAgent =
+      samplingContext.request?.headers["user-agent"] ||
+      samplingContext.request?.headers["User-Agent"];
+    // Exclude traces if user agent is kube-probe
+    if (userAgent && userAgent.includes("kube-probe")) {
+      return 0.0;
+    }
+    // Default sample rate for all other traces
+    return 0.1; // Capture 10% of the transactions
+  },
 });
