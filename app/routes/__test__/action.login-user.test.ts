e05cb083e1d0190930df6507426d4418edaca15e
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-09-23 16:00:33 +0200

    Update mockAuth server path in login user tests

--- app/routes/__test__/action.login-user.test.ts
+++ app/routes/__test__/action.login-user.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
-vi.mock("~/services/mockAuth.server", () => ({
+vi.mock("~/mocks/auth/mockAuth.server", () => ({
   loginAsDeveloper: vi.fn(),
 }));
 
