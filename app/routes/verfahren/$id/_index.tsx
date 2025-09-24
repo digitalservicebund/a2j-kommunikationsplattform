d2f04f2afe5145bcf67b9a125a7173ebbf32eebe
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-09-24 16:10:43 +0200

    Add warning Alert to Verfahrendetails route

--- app/routes/verfahren/$id/_index.tsx
+++ app/routes/verfahren/$id/_index.tsx
@@ -12,14 +12,32 @@ export default function Verfahrensdetails({
   loaderData,
 }: Route.ComponentProps) {
   const { data } = loaderData;
+
   return (
-    <div>
-      <h1 className="kern-heading-large mb-kern-space-large">
-        Verfahrensdetails
-      </h1>
-      <div className="flex">
+    <>
+      <h1 className="kern-heading-large">Verfahrensdetails</h1>
+
+      <div className="my-kern-space-large gap-y-kern-space-large flex flex-col">
+        <div
+          className="kern-alert kern-alert--warning my-kern-space-default"
+          role="alert"
+        >
+          <div className="kern-alert__header">
+            <span
+              className="kern-icon kern-icon--warning kern-icon--small"
+              aria-hidden
+            ></span>
+            <span className="kern-title">Work in progress</span>
+          </div>
+          <div className="kern-alert__body">
+            <p className="kern-body">
+              Diese Seite ist noch in der Entwicklung. Darstellungen und Layouts
+              können sich jederzeit ändern.
+            </p>
+          </div>
+        </div>
         <VerfahrenTile {...data} />
       </div>
-    </div>
+    </>
   );
 }
