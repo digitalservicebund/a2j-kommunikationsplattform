18791945bbdbc8bcb326af6e0156024046d546f2
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-09-24 16:09:55 +0200

    Remove unstarted work

--- app/routes/verfahren/_index.tsx
+++ app/routes/verfahren/_index.tsx
@@ -14,15 +14,12 @@ export async function loader({ request }: Route.LoaderArgs) {
 
 export default function Verfahren({ loaderData }: Route.ComponentProps) {
   const { verfahren } = loaderData;
+
   return (
     <>
-      {/* TODO: For future reference, add new components here  */}
       <h1 className="kern-heading-large">Verfahren</h1>
-      <hr className="kern-divider" aria-hidden />
-      <h1 className="kern-heading-large">Suche</h1>
-      <hr className="kern-divider my-kern-space-large" aria-hidden />
 
-      <div className="mb-kern-space-large gap-y-kern-space-large flex flex-col">
+      <div className="my-kern-space-large gap-y-kern-space-large flex flex-col">
         {verfahren.map((data) => (
           <VerfahrenTile key={data.id} {...data} />
         ))}
