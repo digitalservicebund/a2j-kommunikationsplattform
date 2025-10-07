faeb35a36c9f6c2db8f91d80f32e282012be24cf
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-10-07 18:30:44 +0200

    Add skeleton loading state for VerfahrenTile

--- app/routes/verfahren/_index.tsx
+++ app/routes/verfahren/_index.tsx
@@ -1,7 +1,15 @@
-import { LoaderFunctionArgs, useLoaderData } from "react-router";
+import { LoaderFunctionArgs, useLoaderData, useNavigation } from "react-router";
 import VerfahrenTile from "~/components/VerfahrenTile";
 import { withSessionLoader } from "~/services/auth/withSessionLoader";
 import fetchVerfahren from "~/services/verfahren/fetchVerfahren.server";
+
+// Number of skeletons per page (could change in the future)
+const SKELETONS = [
+  { id: "skeleton-1" },
+  { id: "skeleton-2" },
+  { id: "skeleton-3" },
+  { id: "skeleton-4" },
+];
 
 export const loader = withSessionLoader(
   async ({ request }: LoaderFunctionArgs) => {
@@ -21,15 +29,47 @@ export default function Verfahren() {
     verfahren: Awaited<ReturnType<typeof fetchVerfahren>>;
   }>();
 
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <>
       <h1 className="kern-heading-large">Verfahren</h1>
-
       <div className="my-kern-space-large gap-y-kern-space-large flex flex-col">
-        {verfahren.map((data) => (
-          <VerfahrenTile key={data.id} {...data} />
-        ))}
+        {isLoading
+          ? SKELETONS.map((s) => <VerfahrenTileSkeleton key={s.id} />)
+          : verfahren.map((data) => <VerfahrenTile key={data.id} {...data} />)}
       </div>
     </>
   );
 }
+const SkeletonBlock = () => (
+  <div className="space-y-kern-space-x-large w-full">
+    <div className="space-y-kern-space-small">
+      <div className="bg-kern-feedback-loader-background h-kern-dimension-large rounded-kern-default w-352"></div>
+      <div className="bg-kern-feedback-loader-background h-kern-dimension-large rounded-kern-default w-224"></div>
+    </div>
+    <div className="space-y-kern-space-small">
+      <div className="bg-kern-feedback-loader-background h-kern-dimension-large rounded-kern-default w-352"></div>
+      <div className="bg-kern-feedback-loader-background h-kern-dimension-large rounded-kern-default w-224"></div>
+    </div>
+  </div>
+);
+
+const VerfahrenTileSkeleton = () => {
+  return (
+    <div className="gap-kern-space-large py-kern-dimension-large px-kern-space-default border-kern-layout-border rounded-kern-default pulse flex w-full flex-col items-center border">
+      <div className="flex w-full">
+        <SkeletonBlock />
+        <SkeletonBlock />
+        <SkeletonBlock />
+      </div>
+      <div className="bg-kern-feedback-loader-background rounded-kern-default h-1 w-full"></div>
+      <div className="flex w-full">
+        <div className="space-y-kern-space-x-large w-full">
+          <div className="bg-kern-feedback-loader-background h-kern-dimension-x-large rounded-kern-default w-352"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
