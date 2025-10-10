b5d914e3c84be6321474c06236b1333c4eaf7354
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-10-10 17:07:22 +0200

    Wrap Header in a <header/> element

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,16 +7,14 @@ import UserProfile from "~/components/UserProfile.static";
 import { useTranslations } from "~/services/translations/context";
 
 const HeaderWrapper = ({ children }: { children: ReactNode }) => (
-  <header>
-    <div className="kern-container">
-      <div className="gap-kern-space-small pt-kern-space-large flex flex-col">
-        {children}
-      </div>
-      <div className="py-kern-space-large">
-        <hr className="kern-divider" aria-hidden="true" />
-      </div>
+  <div className="kern-container">
+    <div className="gap-kern-space-small pt-kern-space-large flex flex-col">
+      {children}
     </div>
-  </header>
+    <div className="py-kern-space-large">
+      <hr className="kern-divider" aria-hidden="true" />
+    </div>
+  </div>
 );
 
 export default function Header({
@@ -30,7 +28,7 @@ export default function Header({
   const { buttons } = useTranslations();
 
   return (
-    <>
+    <header>
       <Kopfzeile />
       {isContentPage ? (
         <HeaderWrapper>
@@ -68,6 +66,6 @@ export default function Header({
           </HeaderWrapper>
         )
       )}
-    </>
+    </header>
   );
 }
