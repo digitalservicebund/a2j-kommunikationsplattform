c27aa50747ada40c5cd2f197c5b29b7701825a95
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-10-10 17:18:45 +0200

    Add tests to ensure <header> is rendered

--- app/components/__test__/Header.test.tsx
+++ app/components/__test__/Header.test.tsx
@@ -32,6 +32,10 @@ vi.mock("react-router", async () => {
   };
 });
 
+function expectHeaderToBePresent(container: HTMLElement) {
+  expect(container.querySelector("header")).toBeInTheDocument();
+}
+
 describe("Header", () => {
   let container: HTMLElement;
   const { labels } = getTestTranslations();
@@ -41,104 +45,115 @@ describe("Header", () => {
     vi.clearAllMocks();
   });
 
-  describe("when user is NOT logged in and is NOT on content page", () => {
-    beforeEach(() => {
-      ({ container } = renderWithTestTranslations(
-        <MemoryRouter>
-          <Header userIsLoggedIn={false} isContentPage={false} />,
-        </MemoryRouter>,
-      ));
+  describe("Header when user is NOT logged in", () => {
+    describe("and is NOT on content page", () => {
+      beforeEach(() => {
+        ({ container } = renderWithTestTranslations(
+          <MemoryRouter>
+            <Header userIsLoggedIn={false} isContentPage={false} />,
+          </MemoryRouter>,
+        ));
+      });
+      it("should render <header>", () => {
+        expectHeaderToBePresent(container);
+      });
+      it("should render Kopfzeile", () => {
+        expect(container.querySelector(".kern-kopfzeile")).toBeInTheDocument();
+      });
+      it("should not render header's Logo, UserProfile or Navigation ", () => {
+        expect(
+          container.querySelector(".kern-icon--network_node"),
+        ).not.toBeInTheDocument();
+        expect(container.querySelector("nav")).not.toBeInTheDocument();
+        expect(container).not.toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
+      });
     });
-    it("should not render <header>", () => {
-      expect(container.querySelector("header")).not.toBeInTheDocument();
-    });
-    it("should render Kopfzeile", () => {
-      expect(container.querySelector(".kern-kopfzeile")).toBeInTheDocument();
-    });
-    it("should not render header's Logo, UserProfile or Navigation ", () => {
-      expect(
-        container.querySelector(".kern-icon--network_node"),
-      ).not.toBeInTheDocument();
-      expect(container.querySelector("nav")).not.toBeInTheDocument();
-      expect(container).not.toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
+
+    describe("and IS on content page", () => {
+      beforeEach(() => {
+        ({ container } = renderWithTestTranslations(
+          <MemoryRouter>
+            <Header userIsLoggedIn={false} isContentPage={true} />,
+          </MemoryRouter>,
+        ));
+      });
+      it("should render <header>", () => {
+        expectHeaderToBePresent(container);
+      });
+      it("should render only Logo and Anmelden button, but no Navigation or UserProfile", () => {
+        expect(
+          container.querySelector(".kern-icon--network_node"),
+        ).toBeInTheDocument();
+        expect(container).toHaveTextContent(buttons.ANMELDEN_BUTTON);
+        expect(container.querySelector("nav")).not.toBeInTheDocument();
+        expect(container).not.toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
+      });
+      it('should render Anmelden button and, when clicked, navigate to "/login" ', () => {
+        const button = container.querySelector("button") as HTMLButtonElement;
+        expect(container.querySelector("button")).toHaveTextContent(
+          buttons.ANMELDEN_BUTTON,
+        );
+        button.click();
+        expect(mockNavigate).toHaveBeenCalledWith("/login");
+      });
     });
   });
 
-  describe("when user is NOT logged in and IS on content page", () => {
-    beforeEach(() => {
-      ({ container } = renderWithTestTranslations(
-        <MemoryRouter>
-          <Header userIsLoggedIn={false} isContentPage={true} />,
-        </MemoryRouter>,
-      ));
+  describe("Header when user IS logged in", () => {
+    describe("and is NOT on content page", () => {
+      beforeEach(() => {
+        ({ container } = renderWithTestTranslations(
+          <MemoryRouter>
+            <Header userIsLoggedIn={true} isContentPage={false} />,
+          </MemoryRouter>,
+        ));
+      });
+      it("should render <header>", () => {
+        expectHeaderToBePresent(container);
+      });
+      it("should render Kopfzeile", () => {
+        expect(container.querySelector(".kern-kopfzeile")).toBeInTheDocument();
+      });
+      it("should not render header's Logo, UserProfile or Navigation ", () => {
+        expect(
+          container.querySelector(".kern-icon--network_node"),
+        ).toBeInTheDocument();
+        expect(container.querySelector("nav")).toBeInTheDocument();
+        expect(container).toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
+      });
     });
-    it("should render only Logo and Anmelden button and no Navigation or UserProfile", () => {
-      expect(
-        container.querySelector(".kern-icon--network_node"),
-      ).toBeInTheDocument();
-      expect(container).toHaveTextContent(buttons.ANMELDEN_BUTTON);
-      expect(container.querySelector("nav")).not.toBeInTheDocument();
-      expect(container).not.toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
-    });
-    it('should render Anmelden button and, when clicked, navigate to "/login" ', () => {
-      const button = container.querySelector("button") as HTMLButtonElement;
-      expect(container.querySelector("button")).toHaveTextContent(
-        buttons.ANMELDEN_BUTTON,
-      );
-      button.click();
-      expect(mockNavigate).toHaveBeenCalledWith("/login");
-    });
-  });
 
-  describe("when user IS logged in and is NOT on content page", () => {
-    beforeEach(() => {
-      ({ container } = renderWithTestTranslations(
-        <MemoryRouter>
-          <Header userIsLoggedIn={true} isContentPage={false} />,
-        </MemoryRouter>,
-      ));
-    });
-    it("should render <header>", () => {
-      expect(container.querySelector("header")).toBeInTheDocument();
-    });
-    it("should render Kopfzeile", () => {
-      expect(container.querySelector(".kern-kopfzeile")).toBeInTheDocument();
-    });
-    it("should not render header's Logo, UserProfile or Navigation ", () => {
-      expect(
-        container.querySelector(".kern-icon--network_node"),
-      ).toBeInTheDocument();
-      expect(container.querySelector("nav")).toBeInTheDocument();
-      expect(container).toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
-    });
-  });
+    describe("and IS on content page", () => {
+      beforeEach(() => {
+        ({ container } = renderWithTestTranslations(
+          <MemoryRouter>
+            <Header userIsLoggedIn={true} isContentPage={true} />,
+          </MemoryRouter>,
+        ));
+      });
 
-  describe("when user IS logged in and IS on content page", () => {
-    beforeEach(() => {
-      ({ container } = renderWithTestTranslations(
-        <MemoryRouter>
-          <Header userIsLoggedIn={true} isContentPage={true} />,
-        </MemoryRouter>,
-      ));
-    });
-    it("should render Logo and back button, but not Navigation or UserProfile", () => {
-      expect(
-        container.querySelector(".kern-icon--arrow-back"),
-      ).toBeInTheDocument();
-      expect(container).toHaveTextContent(buttons.BACK_BUTTON);
-      expect(
-        container.querySelector(".kern-icon--network_node"),
-      ).toBeInTheDocument();
-      expect(container.querySelector("nav")).not.toBeInTheDocument();
-      expect(container).not.toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
-    });
-    it("should render Zurück button and, when clicked, navigate back one step in history", () => {
-      const button = container.querySelector("button") as HTMLButtonElement;
-      expect(container.querySelector("button")).toHaveTextContent(
-        buttons.BACK_BUTTON,
-      );
-      button.click();
-      expect(mockNavigate).toHaveBeenCalledWith(-1);
+      it("should render <header>", () => {
+        expectHeaderToBePresent(container);
+      });
+      it("should render Logo and back button, but not Navigation or UserProfile", () => {
+        expect(
+          container.querySelector(".kern-icon--arrow-back"),
+        ).toBeInTheDocument();
+        expect(container).toHaveTextContent(buttons.BACK_BUTTON);
+        expect(
+          container.querySelector(".kern-icon--network_node"),
+        ).toBeInTheDocument();
+        expect(container.querySelector("nav")).not.toBeInTheDocument();
+        expect(container).not.toHaveTextContent(labels.LOGGED_IN_AS_LABEL);
+      });
+      it("should render Zurück button and, when clicked, navigate back one step in history", () => {
+        const button = container.querySelector("button") as HTMLButtonElement;
+        expect(container.querySelector("button")).toHaveTextContent(
+          buttons.BACK_BUTTON,
+        );
+        button.click();
+        expect(mockNavigate).toHaveBeenCalledWith(-1);
+      });
     });
   });
 });
