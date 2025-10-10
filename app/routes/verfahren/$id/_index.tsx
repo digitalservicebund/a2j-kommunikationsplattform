0e39f7245c13f2e85d9e732e37207bc8020b0494
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-10-10 16:34:32 +0200

    Refactor suspense for verfahren details

--- app/routes/verfahren/$id/_index.tsx
+++ app/routes/verfahren/$id/_index.tsx
@@ -1,16 +1,16 @@
-import { LoaderFunctionArgs, useLoaderData } from "react-router";
+import { Suspense } from "react";
+import { Await, LoaderFunctionArgs, useLoaderData } from "react-router";
 import VerfahrenTileSkeleton from "~/components/skeletons/VerfahrenTileSkeleton.static";
 import VerfahrenTile from "~/components/VerfahrenTile";
 import WorkInProgressAlert from "~/components/WorkInProgressAlert.static";
 import { withSessionLoader } from "~/services/auth/withSessionLoader";
 import fetchVerfahrenById from "~/services/verfahren/fetchVerfahrenById.server";
-import type { Route } from "../../../../.react-router/types/app/routes/verfahren/$id/+types/_index";
 
 export const loader = withSessionLoader(
   async ({ params }: LoaderFunctionArgs) => {
     if (params.id) {
       return {
-        data: await fetchVerfahrenById({ id: params.id }),
+        data: fetchVerfahrenById({ id: params.id }),
       };
     } else {
       throw new Response(
@@ -23,35 +23,21 @@ export const loader = withSessionLoader(
   },
 );
 
-export async function clientLoader({ serverLoader }: Route.ClientLoaderArgs) {
-  return await serverLoader();
-}
-
-// Enable client-side hydration for this loader.
-// This ensures that HydrateFallback is shown during loading and the data is rehydrated after navigation.
-clientLoader.hydrate = true as const;
-
-export function HydrateFallback() {
-  return (
-    <>
-      <h1 className="kern-heading-large">Verfahrensdetails</h1>
-      <div className="my-kern-space-large gap-y-kern-space-large flex flex-col">
-        <VerfahrenTileSkeleton />
-      </div>
-    </>
-  );
-}
-
 export default function Verfahrendetails() {
   const { data } = useLoaderData<{
-    data: Awaited<ReturnType<typeof fetchVerfahrenById>>;
+    data: Promise<ReturnType<typeof fetchVerfahrenById>>;
   }>();
+
   return (
     <>
       <h1 className="kern-heading-large">Verfahrensdetails</h1>
       <WorkInProgressAlert />
       <div className="my-kern-space-large gap-y-kern-space-large flex flex-col">
-        <VerfahrenTile {...data} />
+        <Suspense fallback={<VerfahrenTileSkeleton />}>
+          <Await resolve={data}>
+            {(resolvedData) => <VerfahrenTile {...resolvedData} />}
+          </Await>
+        </Suspense>
       </div>
     </>
   );
