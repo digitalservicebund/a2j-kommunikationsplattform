b4c444359c85317566697bd03f6613319c7c08ff
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-10-14 15:18:18 +0200

    Refactor VerfahrenTileSkeleton to fix width

--- app/components/skeletons/VerfahrenTileSkeleton.static.tsx
+++ app/components/skeletons/VerfahrenTileSkeleton.static.tsx
@@ -1,3 +1,5 @@
+import { clsx } from "clsx";
+
 const SkeletonBlock = () => (
   <div className="space-y-kern-space-x-large w-full">
     <div className="space-y-kern-space-small">
@@ -12,17 +14,25 @@ const SkeletonBlock = () => (
 );
 
 export default function VerfahrenTileSkeleton() {
+  const cssClasses = clsx(
+    "relative",
+    "after:border-y-1 sm:after:border-x-1 sm:after:rounded-kern-default after:border-kern-layout-border",
+    "after:absolute after:-z-1 after:top-0 after:-right-16 after:bottom-0 after:-left-16",
+  );
+
   return (
-    <div className="gap-kern-space-large py-kern-dimension-large px-kern-space-default border-kern-layout-border rounded-kern-default flex w-full animate-pulse flex-col items-center border">
-      <div className="space-x-kern-space-small flex w-full">
-        <SkeletonBlock />
-        <SkeletonBlock />
-        <SkeletonBlock />
-      </div>
-      <div className="bg-kern-feedback-loader-background rounded-kern-default h-1 w-full"></div>
-      <div className="flex w-full">
-        <div className="space-y-kern-space-x-large w-full">
-          <div className="bg-kern-feedback-loader-background h-kern-dimension-x-large rounded-kern-default w-352"></div>
+    <div className={cssClasses}>
+      <div className="gap-kern-space-large py-kern-dimension-large px-kern-space-default flex w-full animate-pulse flex-col items-center">
+        <div className="space-x-kern-space-small flex w-full">
+          <SkeletonBlock />
+          <SkeletonBlock />
+          <SkeletonBlock />
+        </div>
+        <div className="bg-kern-feedback-loader-background rounded-kern-default h-1 w-full"></div>
+        <div className="flex w-full">
+          <div className="space-y-kern-space-x-large w-full">
+            <div className="bg-kern-feedback-loader-background h-kern-dimension-x-large rounded-kern-default w-352"></div>
+          </div>
         </div>
       </div>
     </div>
