6840482a13c5d06d875451f601375ba6f1d8ff77
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-10-27 15:41:56 +0100

    Refactor WorkInProgressAlert to use Alert

--- app/components/WorkInProgressAlert.static.tsx
+++ app/components/WorkInProgressAlert.static.tsx
@@ -1,22 +1,13 @@
+import Alert from "~/components/Alert";
 import { useTranslations } from "~/services/translations/context";
 
 export default function WorkInProgressAlert() {
   const { alerts } = useTranslations();
   return (
-    <div
-      className="kern-alert kern-alert--warning my-kern-space-default"
-      role="alert"
-    >
-      <div className="kern-alert__header">
-        <span
-          className="kern-icon kern-icon--warning kern-icon--small"
-          aria-hidden
-        ></span>
-        <span className="kern-title">{alerts.WORK_IN_PROGRESS_TITLE}</span>
-      </div>
-      <div className="kern-alert__body">
-        <p className="kern-body">{alerts.WORK_IN_PROGRESS_MESSAGE}</p>
-      </div>
-    </div>
+    <Alert
+      type="warning"
+      title={alerts.WORK_IN_PROGRESS_TITLE}
+      message={alerts.WORK_IN_PROGRESS_MESSAGE}
+    />
   );
 }
