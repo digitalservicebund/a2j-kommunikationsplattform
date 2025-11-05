3635ce6264ab3532e2b0192773b5c61f54383ae3
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2025-11-05 12:39:16 +0100

    Wrap logo in a link for better navigation

--- app/components/Logo.static.tsx
+++ app/components/Logo.static.tsx
@@ -4,12 +4,16 @@ export default function Logo() {
   // Using German labels as default/static labels
   const { labels } = dictionaries["de"];
   return (
-    <div className="gap-kern-space-small flex flex-row flex-wrap items-center break-all">
+    <a
+      aria-label={labels.TO_START_PAGE_LABEL}
+      href="/"
+      className="gap-kern-space-small kern-link visited:text-kern-action-default flex flex-row flex-wrap items-center break-all no-underline"
+    >
       <span
-        className="kern-icon kern-icon--network_node kern-icon--large"
+        className="kern-icon kern-icon--network_node kern-icon--large bg-current"
         aria-hidden
       />
-      <h1 className="kern-heading-large">{labels.LOGO_LABEL}</h1>
-    </div>
+      <div className="kern-heading-large">{labels.LOGO_LABEL}</div>
+    </a>
   );
 }
