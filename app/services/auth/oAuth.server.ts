49a76f563b7e83841b1d3cb7a7cddc3770967348
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-11-25 08:29:10 +0100

    Add refresh token handling for BRAK IdP

--- app/services/auth/oAuth.server.ts
+++ app/services/auth/oAuth.server.ts
@@ -1,7 +1,11 @@
 import { Authenticator } from "remix-auth";
 import { CodeChallengeMethod, OAuth2Strategy } from "remix-auth-oauth2";
 import { serverConfig } from "~/config/config.server";
-import { createUserSession } from "./session.server";
+import {
+  createUserSession,
+  getUserSession,
+  updateUserSession,
+} from "./session.server";
 
 export interface AuthenticationContext {
   accessToken: string;
@@ -20,52 +24,112 @@ export enum AuthenticationProvider {
 
 // BRAK IdP uses "Authorization Code" OAuth 2.0 flow
 export const authenticator = new Authenticator<AuthenticationResponse>();
-authenticator.use(
-  new OAuth2Strategy(
-    {
-      cookie: "oauth2",
-      clientId: serverConfig().BRAK_IDP_OIDC_CLIENT_ID,
-      clientSecret: serverConfig().BRAK_IDP_OIDC_CLIENT_SECRET,
-      authorizationEndpoint: `${serverConfig().BRAK_IDP_OIDC_ISSUER}/protocol/openid-connect/auth`,
-      tokenEndpoint: `${serverConfig().BRAK_IDP_OIDC_ISSUER}/protocol/openid-connect/token`,
-      redirectURI: `${serverConfig().BRAK_IDP_OIDC_REDIRECT_URI}`,
-      scopes: ["openid"], // as required by BRAK IdP
-      codeChallengeMethod: CodeChallengeMethod.S256,
-    },
-    async ({ tokens, request }) => {
-      // @TODO: remove
-      console.log(
-        "BRAK IdP OAuth2 debugging: hasRefreshToken",
-        tokens.hasRefreshToken(),
-        "BRAK access token expires in",
-        tokens.accessTokenExpiresInSeconds(),
-        "BRAK refresh token",
-        tokens.refreshToken(),
-      );
 
-      const accessToken = tokens.accessToken();
-      const expiresAt =
-        Date.now() + tokens.accessTokenExpiresInSeconds() * 1000; // 300 seconds
-      const refreshToken = tokens.refreshToken();
+const oauth2Strategy = new OAuth2Strategy(
+  {
+    cookie: "oauth2",
+    clientId: serverConfig().BRAK_IDP_OIDC_CLIENT_ID,
+    clientSecret: serverConfig().BRAK_IDP_OIDC_CLIENT_SECRET,
+    authorizationEndpoint: `${serverConfig().BRAK_IDP_OIDC_ISSUER}/protocol/openid-connect/auth`,
+    tokenEndpoint: `${serverConfig().BRAK_IDP_OIDC_ISSUER}/protocol/openid-connect/token`,
+    redirectURI: `${serverConfig().BRAK_IDP_OIDC_REDIRECT_URI}`,
+    scopes: ["openid"], // as required by BRAK IdP
+    codeChallengeMethod: CodeChallengeMethod.S256,
+  },
+  async ({ tokens, request }) => {
+    // @TODO: remove
+    console.log(
+      "Authenticated via BRAK IdP. OAuth2 debugging: hasRefreshToken",
+      tokens.hasRefreshToken(),
+      "BRAK access token expires in",
+      tokens.accessTokenExpiresInSeconds(),
+      "BRAK refresh token",
+      tokens.refreshToken(),
+    );
 
-      const sessionCookieHeader = await createUserSession(
+    const acessTokenExpiresInSeconds = tokens.accessTokenExpiresInSeconds();
+    const accessToken = tokens.accessToken();
+    const expiresAt = Date.now() + acessTokenExpiresInSeconds * 1000; // 300 seconds
+    const refreshToken = tokens.refreshToken();
+
+    const sessionCookieHeader = await createUserSession(
+      accessToken,
+      expiresAt,
+      refreshToken,
+      request,
+    );
+
+    const response: AuthenticationResponse = {
+      authenticationContext: {
         accessToken,
         expiresAt,
         refreshToken,
-        request,
-      );
+      },
+      sessionCookieHeader,
+    };
 
-      const response: AuthenticationResponse = {
-        authenticationContext: {
-          accessToken,
-          expiresAt,
-          refreshToken,
-        },
-        sessionCookieHeader,
-      };
+    scheduleRefreshAccessToken(request);
 
-      return response;
-    },
-  ),
-  AuthenticationProvider.BEA,
+    return response;
+  },
 );
+
+authenticator.use(oauth2Strategy, AuthenticationProvider.BEA);
+
+let refreshTimeout: NodeJS.Timeout | null = null;
+const refreshMarginInSeconds: number = 30;
+
+const clearRefreshTimeout = () => {
+  if (refreshTimeout) clearTimeout(refreshTimeout);
+  console.log("clear refresh timeout");
+
+  refreshTimeout = null;
+};
+
+async function scheduleRefreshAccessToken(request: Request) {
+  console.log("schedule access token refresh");
+
+  clearRefreshTimeout();
+  const session = await getUserSession(request);
+  if (!session) return;
+  const delay = session.expiresAt - Date.now() - refreshMarginInSeconds * 1000;
+  refreshTimeout = setTimeout(
+    async () => {
+      await refreshAccessToken(request);
+    },
+    Math.max(delay, 0),
+  );
+}
+
+async function refreshAccessToken(request: Request) {
+  console.log("refresh access token");
+
+  const session = await getUserSession(request);
+  if (!session) throw new Error("No session available that could be updated");
+
+  try {
+    const newTokens = await oauth2Strategy.refreshToken(session.refreshToken);
+
+    console.log(
+      "BRAK access token was refreshed. hasRefreshToken",
+      newTokens.hasRefreshToken(),
+      "BRAK access token expires in",
+      newTokens.accessTokenExpiresInSeconds(),
+      "BRAK refresh token",
+      newTokens.refreshToken(),
+    );
+
+    scheduleRefreshAccessToken(request);
+
+    return await updateUserSession({
+      accessToken: newTokens.accessToken(),
+      refreshToken: newTokens.hasRefreshToken()
+        ? newTokens.refreshToken()
+        : session.refreshToken,
+      expiresAt: Date.now() + newTokens.accessTokenExpiresInSeconds() * 1000,
+      request,
+    });
+  } catch (e) {
+    console.error("Refresh failed", e);
+  }
+}
