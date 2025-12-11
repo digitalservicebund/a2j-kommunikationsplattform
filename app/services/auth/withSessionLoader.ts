e8338187b4645c14b8f9ac712fcd4c39cbdf9537
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-12-11 20:38:15 +0100

    Revert set cookie impl. within withSessionLoader
    This should fix the current access token refresh handling error.
    Access handling will be improved within a new story: https://digitalservicebund.atlassian.net/browse/KOMMPLA-941

--- app/services/auth/withSessionLoader.ts
+++ app/services/auth/withSessionLoader.ts
@@ -10,36 +10,7 @@ export function withSessionLoader(
   return async (args) => {
     const { request } = args;
     const userSession = await requireUserSession(request);
-    const result = await loaderFn({
-      ...args,
-      userSession: userSession.authenticationTokens,
-    });
-
-    if (userSession.sessionCookieHeader) {
-      const headers = new Headers(
-        result instanceof Response ? result.headers : {},
-      );
-      headers.set("Set-Cookie", userSession.sessionCookieHeader);
-
-      if (result instanceof Response) {
-        console.log("response with body, instanceof Response");
-        return new Response(result.body, {
-          status: result.status,
-          statusText: result.statusText,
-          headers,
-        });
-      }
-
-      console.log("response with status 200");
-      return new Response(JSON.stringify(result), {
-        status: 200,
-        headers: {
-          ...headers,
-          "Content-Type": "application/json",
-        },
-      });
-    }
-
-    return result;
+    console.log("withSessionLoader userSession:", userSession);
+    return loaderFn({ ...args, userSession: userSession.authenticationTokens });
   };
 }
