547ca2306e7ebf5504e955cf6f9ef381858f4e40
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2025-12-12 18:21:28 +0100

    Remove comments from bearer token

--- app/services/auth/getBearerToken.server.ts
+++ app/services/auth/getBearerToken.server.ts
@@ -7,12 +7,10 @@ import { getUserSession } from "./session.server";
  * @see: https://sergiodxa.com/articles/working-with-refresh-tokens-in-remix
  */
 export async function getBearerToken(request: Request): Promise<string> {
-  console.log("getBearerToken", request);
+  console.log("getBearerToken");
 
   const userSession = await getUserSession(request);
-  console.log("userSession", userSession);
   const accessToken = userSession.authenticationTokens.accessToken;
-  console.log("accessToken", accessToken);
   const token = await authorizeToken(accessToken);
   return token.access_token;
 }
