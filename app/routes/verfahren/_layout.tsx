84a3c4f3af97bd1de80f1fc7f67ec505ee5f870b
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2026-01-14 11:50:48 +0100

    Add middleware setup

--- app/routes/verfahren/_layout.tsx
+++ app/routes/verfahren/_layout.tsx
@@ -1,11 +1,37 @@
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
 import { MatchHandle } from "~/components/Breadcrumbs";
+import { authContext } from "~/services/auth/auth.context";
+import { getAuthData } from "~/services/auth/authSession.server";
+import type { Route } from "./+types/_layout";
 
 export const handle: MatchHandle = {
   breadcrumb: {
     title: "Verfahren",
   },
 };
+
+export const middleware: Route.MiddlewareFunction[] = [
+  async ({ request, context }, next) => {
+    console.log("MW TRIGGERED FOR VERFAHREN LAYOUT");
+    const authSession = await getAuthData(request);
+
+    if (!authSession) throw redirect("/login");
+
+    context.set(authContext, authSession);
+
+    const response = await next();
+
+    if (authSession.sessionCookieHeader) {
+      response.headers.append("Set-Cookie", authSession.sessionCookieHeader);
+    }
+
+    return response;
+  },
+];
+
+export function loader() {
+  return null;
+}
 
 export default function VerfahrenLayout() {
   return <Outlet />;
