e2d9473fd29dc0757da1b5fd9deb231454fa84c9
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2026-06-04 17:44:06 +0200

    Update auth dev login with idToken

--- app/services/auth/loginAsDeveloper.server.ts
+++ app/services/auth/loginAsDeveloper.server.ts
@@ -6,10 +6,12 @@ export const loginAsDeveloper = async (request: Request) => {
     console.log("loginAsDeveloper");
 
     const devAccessToken = "dev-access-token";
+    const devIdToken = "DE.BRAK_SPT.cbceac6e-5fe8-11f1-b4b1-325096b39f47.28e6";
     const expiresAt = Date.now() + 60 * 60 * 1000 * 24 * 14; // 14 days
     const devRefreshToken = "dev-refresh-token";
     const sessionCookieHeader = await setAuthSession({
       accessToken: devAccessToken,
+      idToken: devIdToken,
       expiresAt,
       refreshToken: devRefreshToken,
       request,
