5d71d1c5d39896fc905b1f0ce099de3251afee57
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2026-07-02 11:14:56 +0200

    Fix e2e test webServer config
    - With https://github.com/digitalservicebund/a2j-kommunikationsplattform/pull/653
    the npm run dev script was removed and replaced with npm run dev:op
    - Therefor, e2e test config has been upgraded with npm start usage

--- playwright.config.ts
+++ playwright.config.ts
@@ -93,7 +93,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
