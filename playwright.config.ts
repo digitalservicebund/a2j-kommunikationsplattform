942a2c64cd948dc14243d756c07b22dfd3dba546
Manuel Puchta <manuel.puchta@digitalservice.bund.de>
2026-07-02 11:31:48 +0200

    Remove webServer from e2e test

--- playwright.config.ts
+++ playwright.config.ts
@@ -92,9 +92,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm start",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "npm start", // "npm run dev:op"
+  //   url: "http://localhost:3000",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
