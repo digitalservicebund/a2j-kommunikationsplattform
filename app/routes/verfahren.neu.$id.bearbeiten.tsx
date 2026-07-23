756d97209874fec9acecb46fb05a7f258593649f
Manuel von Stuckrad <manuelpuchta@pm.me>
2026-07-23 12:13:13 +0200

    Undo change

--- app/routes/verfahren.neu.$id.bearbeiten.tsx
+++ app/routes/verfahren.neu.$id.bearbeiten.tsx
@@ -900,7 +900,7 @@ export default function VerfahrenNeuBearbeiten() {
                   </div>
                 </div>
 
-                {/* Verfahren related doc */}
+                {/* Verfahren related docs */}
                 <div className="kern-card">
                   <div className="kern-card__container mb-kern-space-default">
                     <header className="kern-card__header">
