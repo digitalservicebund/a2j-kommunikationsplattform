6d3718fedb40e0c324ba0ae1acabb3c5e6664e61
Rostyslav Kanibolotskyi <rkanibolotskyi@gmail.com>
2026-08-05 11:27:23 +0200

    Add helper functions for response schema

--- app/domains/verfahren/schemas/helpers.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const getListeResponseSchema = <T extends z.ZodTypeAny>(
-  elementSchema: T,
-) =>
-  z.object({
-    list_version: z.string(),
-    elemente: z.array(elementSchema),
-  });
